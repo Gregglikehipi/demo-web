844be84001399778d78d6cd6c2d8d8863d6ad2cc
Gregg <arkhipov.zena@gmail.com>
2023-02-19 21:40:06 +0500

    Change..3

--- src/main/java/com/example/demoweb/model/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/com/example/demoweb/model/Post.java
@@ -1,8 +1,8 @@
 package com.example.demoweb.model.com.example.demoweb.model;
 
 public class Post {
-    private String text;
-    private Integer likes;
+    public String text;
+    public Integer likes;
     public Post(String text, Integer likes) {
         this.text = text;
         this.likes = likes;
